87c90d1e47cf5f1ebdece90f0a2c51528a48a1c6
SamHughesGit <samhughesprof07@gmail.com>
2026-01-26 11:16:21 +0000

    Organisation

--- AlgorithmVisualiser/Sorter.cs
+++ AlgorithmVisualiser/Sorter.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 using System.Text;
 using System.Threading.Tasks;
+using AlgorithmVisualiser.Pages;
+using System.Runtime.CompilerServices;
+using System.Drawing;
 
 namespace AlgorithmVisualiser
 {
-    class Sorter
+    // Base clas sorters derive from
+    public abstract class Sort
     {
+        public string BigO { get; set; }
+        public abstract void SortElements(List<Rectangle> elements);
+
+    }
+
+    static class Sorter
+    {
+        // List of pages for dynamically creating buttons
+        static public List<Page> pages = new List<Page> { new BubbleSortPage() };
+
+        class BubbleSort : Sort
+        {
+            // Constructor
+            public BubbleSort()
+            {
+                this.BigO = "O(n^2)";
+            }
+
+            // Perform the actual sort
+            public override void SortElements(List<Rectangle> elements)
+            {
+                
+            }
+        }
+
+        class InsertionSort : Sort
+        {
+            // Constructor 
+            public InsertionSort()
+            {
+                this.BigO = "O(n^2)";
+            }
+
+            // Perform the actual sort
+            public override void SortElements(List<Rectangle> elements)
+            {
+
+            }
+        }
     }
 }
