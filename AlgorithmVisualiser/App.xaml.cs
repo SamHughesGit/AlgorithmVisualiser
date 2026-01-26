214638c685aa8d8aa382b222dd88744f16b9c6eb
SamHughesGit <samhughesprof07@gmail.com>
2026-01-26 10:48:55 +0000

    Add project files.

---
+++ AlgorithmVisualiser/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AlgorithmVisualiser;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
